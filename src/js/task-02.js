11018aabe359f962902fac8540eabfabbca04e61
Malwina Knie <malwinaknie@wp.pl>
2022-12-08 22:10:44 +0100

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const findList = document.querySelector('#ingredients');
+
+const list = ingredients.forEach((ingredient)=> {
+  let newList = document.createElement('li');
+  newList.textContent = ingredient;
+  newList.classList.add('item');
+  findList.append(newList);
+  console.log(newList)
+}
+)
+
