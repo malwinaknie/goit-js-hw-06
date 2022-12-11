2bd5b50b4a1e7a23d42d95c10c5eb3fd6109eeb3
Malwina Knie <malwinaknie@wp.pl>
2022-12-11 19:31:17 +0100

    finished task4

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,1 +1,18 @@
-const counterValue = document.querySelector('#value');
+let counterSpan = document.querySelector('#value');
+let counterValue = 0;
+
+const addBtn = document.querySelector('[data-action=increment]');
+const subtractBtn = document.querySelector('[data-action=decrement]');
+
+const incrementation = () => {
+    counterValue ++;
+    counterSpan.textContent = counterValue;
+};
+const decrementation = () => {
+    counterValue --;
+    counterSpan.textContent = counterValue;
+};
+
+addBtn.addEventListener('click', incrementation);
+subtractBtn.addEventListener('click', decrementation);
+
