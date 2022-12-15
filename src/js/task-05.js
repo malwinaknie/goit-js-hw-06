9809f22fb54ac902b1a7cbc4549ef08cac40c71d
Malwina Knie <malwinaknie@wp.pl>
2022-12-16 00:34:09 +0100

    Update task-05.js

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,3 +1,17 @@
-const outputItem = document.querySelector('#name-output');
-const inputItem = document.querySelector('#name-input');
+let inputItem = document.querySelector("#name-input");
+let outputItem = document.querySelector("#name-output");
 
+const activeInput = () => {
+  outputItem.innerHTML = `${inputItem.value}`;
+};
+const blankInput = () => {
+  outputItem.innerHTML = `Anonymous`;
+};
+
+inputItem.addEventListener("input", () => {
+  if (inputItem.value == "") {
+    blankInput();
+  } else {
+    activeInput();
+  }
+});
