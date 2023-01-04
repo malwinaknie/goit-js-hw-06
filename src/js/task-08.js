f60d8156bd8db36c8136d430d2cb24348dff0801
Malwina Knie <malwinaknie@wp.pl>
2023-01-04 23:27:55 +0100

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', (e) => {
+e.preventDefault();
+const {
+    elements: { email, password }
+} = e.currentTarget;
+
+if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!")
+}
+
+console.log(`Email: ${email.value}, Password: ${password.value}`);
+e.currentTarget.reset();
+})
 
