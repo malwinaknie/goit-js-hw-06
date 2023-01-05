64325d1d03305fbf226cee115f6280fcf318c283
Malwina Knie <malwinaknie@wp.pl>
2023-01-05 16:38:35 +0100

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
+const changeBtn = document.querySelector('.change-color');
+const body = document.querySelector('body');
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+changeBtn.addEventListener('click', () => {
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  document.querySelector('.color').textContent = `${getRandomHexColor()}`
+})
