eb437b6c45d4e06d82b00e663c09e13097fbd58d
Malwina Knie <malwinaknie@wp.pl>
2023-01-06 20:38:46 +0100

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,35 @@
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxes = document.getElementById('boxes');
+const input = document.querySelector('input');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const createBoxes = amount => {
+  const elemsToAdd = [];
+  for (let i = 0; i < amount; i++) {
+    const newBox = document.createElement('div');
+    newBox.style.height = `${30 + 10 * i}px`;
+    newBox.style.width = `${30 + 10 * i}px`;
+    newBox.style.backgroundColor = getRandomHexColor();
+    boxes.append(newBox)
+  }
+  return elemsToAdd;
+}
+
+createBtn.addEventListener('click', () => {
+  let boxesToAdd = createBoxes(input.value);
+  boxes.append(...boxesToAdd);
+});
+
+  const destroyBoxes = () => {
+    boxes.innerHTML = ''
+  };
+  
+destroyBtn.addEventListener('click', () => {
+  destroyBoxes.call();
+  input.value = 0;
+});
+
